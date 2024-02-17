fa4224ab47fcdad3f9e4a9a40a0aae3691ec3d78
Madhav M <madhavmanoj117@gmail.com>
2024-02-17 17:48:08 +0000

    restructure plugins

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,31 @@
+return {
+  -- Add treesitter for rust, c++ and go
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "rust",
+        "c++",
+        "go",
+      })
+    end,
+  },
+
+  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
+  { import = "lazyvim.plugins.extras.lang.json" },
+
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "prettier",
+      },
+    },
+  },
+}
