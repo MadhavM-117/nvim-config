9f92d5cff26e26716204c7d26d167537b6023bbe
Madhav M <madhavmanoj117@gmail.com>
2024-06-11 12:11:47 +0100

    add ability to select virtual env

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -79,8 +79,7 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
-  ruff = {},
+  pyright = {},
   rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs'} },
