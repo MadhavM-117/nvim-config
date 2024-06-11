115fb873b258d715ca12290a36065e4e15772163
Madhav M <madhavmanoj117@gmail.com>
2024-06-11 06:39:52 +0100

    add required lsp support

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -80,9 +80,10 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  ruff = {},
+  rust_analyzer = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
