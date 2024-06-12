bf2dc07f7984925ba02a8a9baa1d4bd4f479315d
Madhav M <madhavmanoj117@gmail.com>
2024-06-12 15:26:27 +0100

    add requirements for debugging

--- lua/install-plugins.lua
+++ lua/install-plugins.lua
@@ -135,9 +135,11 @@ require('lazy').setup({
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
-        "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
       },
     },
+
+    { "nvim-neotest/nvim-nio" },
 
     -- require 'kickstart.plugins.autoformat',
     -- require 'kickstart.plugins.debug',
