9a627af08c2dfc3e3f33bbb63fda9fd7d9dcadcc
Madhav M. <madhavmanoj117@gmail.com>
2025-05-26 19:45:38 +0100

    feat: add neotree as fileexplorer

--- lua/plugins/install.lua
+++ lua/plugins/install.lua
@@ -3,5 +3,15 @@ require("lazy").setup({
 	spec = {
 		-- nvim color theme
 		{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+
+		-- File explorer sidebar
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			branch = "v3.x",
+			lazy = false,
+		},
+
+		-- Import configs for plugins
+		{ import = "plugins.config" },
 	},
 })
