ca7e7fd7ccc67a40ed9d3ea19029e73022aea819
Madhav Manoj <madhavmanoj117@gmail.com>
2026-02-11 22:15:40 +0000

    feat: add keymaps for obsidian

--- lua/plugins/standard/obsidian.lua
+++ lua/plugins/standard/obsidian.lua
@@ -1,6 +1,5 @@
 return {
 	"obsidian-nvim/obsidian.nvim",
-	ft = "markdown",
 	---@module 'obsidian'
 	---@type obsidian.config
 	opts = {
@@ -13,7 +12,18 @@ return {
 		},
 
 		daily_notes = {
-			folder = "9-journal"
+			folder = "9-journal",
+			default_tags = nil,
+			workdays_only = false,
 		},
+	},
+	keys = {
+		{ "<leader>o",  nil,                              desc = "Obsidian",                    group = "Obsidian" },
+		-- todo: use a better ui to handle text input for note name.
+		{ "<leader>on", "<cmd>Obsidian new<cr>",          desc = "Obsidian: New Note" },
+		{ "<leader>oo", "<cmd>Obsidian open<cr>",         desc = "Obsidian: Open in Obsidian" },
+		{ "<leader>os", "<cmd>Obsidian quick_switch<cr>", desc = "Obsidian: search by filename" },
+		{ "<leader>oS", "<cmd>Obsidian search<cr>",       desc = "Obsidian: search notes" },
+		{ "<leader>ot", "<cmd>Obsidian today<cr>",        desc = "Obsidian: today" }
 	},
 }
