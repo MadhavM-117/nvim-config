907c8d6d77eeea8ef2891ecea2177217f879af36
Madhav M. <madhavmanoj117@gmail.com>
2026-04-04 21:29:18 +0100

    feat(obsidian): remove lazy loading

--- lua/plugins/standard/obsidian.lua
+++ lua/plugins/standard/obsidian.lua
@@ -1,35 +1,37 @@
 return {
-	"obsidian-nvim/obsidian.nvim",
-	---@module 'obsidian'
-	---@type obsidian.config
-	opts = {
-		preferred_link_style = "markdown",
-		legacy_commands = false, -- this will be removed in the next major release
-		workspaces = {
-			{
-				name = "personal",
-				path = "~/personal/obsidian/raaz/",
-			},
-		},
+  "obsidian-nvim/obsidian.nvim",
+  lazy = false,
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    preferred_link_style = "markdown",
+    legacy_commands = false, -- this will be removed in the next major release
+    workspaces = {
+      {
+        name = "personal",
+        path = "/home/y13/personal/obsidian/raaz/",
+      },
+    },
 
-		checkbox = {
-			create_new = false,
-			order = { " ", "x" }
-		},
+    checkbox = {
+      create_new = false,
+      order = { " ", "x" },
+    },
 
-		daily_notes = {
-			folder = "9-journal",
-			default_tags = nil,
-			workdays_only = false,
-		},
-	},
-	keys = {
-		{ "<leader>o",  nil,                              desc = "Obsidian",                    group = "Obsidian" },
-		-- todo: use a better ui to handle text input for note name.
-		{ "<leader>on", "<cmd>Obsidian new<cr>",          desc = "Obsidian: New Note" },
-		{ "<leader>oo", "<cmd>Obsidian open<cr>",         desc = "Obsidian: Open in Obsidian" },
-		{ "<leader>os", "<cmd>Obsidian quick_switch<cr>", desc = "Obsidian: search by filename" },
-		{ "<leader>oS", "<cmd>Obsidian search<cr>",       desc = "Obsidian: search notes" },
-		{ "<leader>ot", "<cmd>Obsidian today<cr>",        desc = "Obsidian: today" }
-	},
+    daily_notes = {
+      folder = "9-journal",
+      default_tags = nil,
+      workdays_only = false,
+    },
+  },
+  keys = {
+    { "<leader>o", nil, desc = "Obsidian", group = "Obsidian" },
+    -- todo: use a better ui to handle text input for note name.
+    { "<leader>od", "<cmd>Obsidian dailies<cr>", desc = "Obsidian: Dailies Picker" },
+    { "<leader>of", "<cmd>Obsidian follow_link<cr>", desc = "Obsidian: Follow Link" },
+    { "<leader>oo", "<cmd>Obsidian open<cr>", desc = "Obsidian: Open in Obsidian" },
+    { "<leader>os", "<cmd>Obsidian quick_switch<cr>", desc = "Obsidian: search by filename" },
+    { "<leader>oS", "<cmd>Obsidian search<cr>", desc = "Obsidian: search notes" },
+    { "<leader>ot", "<cmd>Obsidian today<cr>", desc = "Obsidian: today" },
+  },
 }
