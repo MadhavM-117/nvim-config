33ba3e1073be2523602bb4e61faa3993605428e8
Madhav Manoj <madhav.manoj@torchbox.com>
2026-04-27 17:09:43 +0100

    fix(treesitter): migrate to nvim-treesitter main branch API
    - replace configs.setup() with FileType autocmd for highlighting/indentation
    - replace ensure_installed with manual install using new config API
    - pin treesitter-textobjects to main branch

--- lua/plugins/standard/treesitter.lua
+++ lua/plugins/standard/treesitter.lua
@@ -1,54 +1,55 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = 'master',
+	branch = 'main',
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require('nvim-treesitter.configs').setup({
-			ensure_installed = {
-				'astro',
-				'bash',
-				'c',
-				'cpp',
-				'diff',
-				'go',
-				'html',
-				'javascript',
-				'jsdoc',
-				'json',
-				'jsonc',
-				'lua',
-				'luadoc',
-				'luap',
-				'markdown',
-				'markdown_inline',
-				'printf',
-				'prisma',
-				'python',
-				'query',
-				'regex',
-				'rust',
-				'toml',
-				'tsx',
-				'typescript',
-				'vim',
-				'vimdoc',
-				'xml',
-				'yaml',
-			},
-
-
-			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-			auto_install = false,
-			-- Install languages synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-			-- List of parsers to ignore installing
-			ignore_install = {},
-			-- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-			modules = {},
-			highlight = { enable = true },
-			indent = { enable = true },
-
+		vim.api.nvim_create_autocmd('FileType', {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
-	end
+
+		local ensureInstalled = {
+			'astro',
+			'bash',
+			'c',
+			'cpp',
+			'diff',
+			'go',
+			'html',
+			'javascript',
+			'jsdoc',
+			'json',
+			'jsonc',
+			'lua',
+			'luadoc',
+			'luap',
+			'markdown',
+			'markdown_inline',
+			'printf',
+			'prisma',
+			'python',
+			'query',
+			'regex',
+			'rust',
+			'toml',
+			'tsx',
+			'typescript',
+			'vim',
+			'vimdoc',
+			'xml',
+			'yaml',
+		}
+		local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+		local parsersToInstall = vim.iter(ensureInstalled)
+			:filter(function(parser)
+				return not vim.tbl_contains(alreadyInstalled, parser)
+			end)
+			:totable()
+		if #parsersToInstall > 0 then
+			require('nvim-treesitter').install(parsersToInstall)
+		end
+	end,
 }
